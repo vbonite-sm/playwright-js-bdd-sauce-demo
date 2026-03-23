9db671c3306a72a3bffe29f1254f900e9a8d5bcc
Vlademir Bonite <vbonite.sm@gmail.com>
2026-03-23 16:26:59 +0700

    refactor(pages): extract ProductsLocators, update inventoryList reference in login steps

--- pages/ProductsPage.js
+++ pages/ProductsPage.js
@@ -1,19 +1,17 @@
 const BasePage = require('./BasePage');
+const { createProductsLocators } = require('./locators/ProductsLocators');
 
 class ProductsPage extends BasePage {
   constructor(page) {
     super(page);
-    this.inventoryList = page.getByTestId('inventory-list');
-    this.cartBadge = page.getByTestId('shopping-cart-badge');
-    this.cartLink = page.getByTestId('shopping-cart-link');
+    this.locators = createProductsLocators(page);
   }
 
   async getAllProducts() {
-    const items = this.page.locator('[data-test="inventory-item"]');
-    const count = await items.count();
+    const count = await this.locators.inventoryItem.count();
     const products = [];
     for (let i = 0; i < count; i++) {
-      const item = items.nth(i);
+      const item = this.locators.inventoryItem.nth(i);
       const name = await item.getByTestId('inventory-item-name').textContent();
       const description = await item.getByTestId('inventory-item-desc').textContent();
       const price = await item.getByTestId('inventory-item-price').textContent();
@@ -23,16 +21,16 @@ class ProductsPage extends BasePage {
   }
 
   async addFirstProductToCart() {
-    await this.page.locator('[data-test^="add-to-cart"]').nth(0).click();
+    await this.locators.addToCartButton.nth(0).click();
   }
 
   async getCartQuantity() {
-    await this.cartBadge.waitFor({ state: 'visible' });
-    return this.cartBadge.textContent();
+    await this.locators.cartBadge.waitFor({ state: 'visible' });
+    return this.locators.cartBadge.textContent();
   }
 
   async goToCart() {
-    await this.cartLink.click();
+    await this.locators.cartLink.click();
   }
 }
 
