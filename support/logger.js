1ef61489f5e89bf4c462328bf8415cc06c96554e
Vlademir Bonite <vbonite.sm@gmail.com>
2026-03-23 16:32:15 +0700

    feat(logging): install winston, create logger singleton

---
+++ support/logger.js
@@ -0,0 +1,9 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: format.combine(format.colorize(), format.simple()),
+  transports: [new transports.Console()],
+});
+
+module.exports = logger;
