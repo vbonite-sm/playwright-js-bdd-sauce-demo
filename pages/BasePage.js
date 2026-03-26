4fde4dbd6b0cefb4bcb1b84dd32e69f3650f76b8
Vlademir Bonite <vbonite.sm@gmail.com>
2026-03-26 16:31:05 +0700

    feat(pages): add wait helpers to BasePage, delegate CartPage isEnabled methods

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -2,6 +2,33 @@ class BasePage {
   constructor(page) {
     this.page = page;
   }
+
+  async waitAndCheckVisible(locator, options = {}) {
+    try {
+      await locator.waitFor({ state: 'visible', ...options });
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async waitAndCheckEnabled(locator, options = {}) {
+    try {
+      await locator.waitFor({ state: 'visible', ...options });
+      return await locator.isEnabled();
+    } catch {
+      return false;
+    }
+  }
+
+  async waitAndCheckHidden(locator, options = {}) {
+    try {
+      await locator.waitFor({ state: 'hidden', ...options });
+      return true;
+    } catch {
+      return false;
+    }
+  }
 }
 
 module.exports = BasePage;
